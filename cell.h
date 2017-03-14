0305d807b1d9426c524e5fcb5a33affd9de1959c
Luc Allio <luc.allio.3@gmail.com>
2017-03-14 19:01:35 +0100

    Update cell.h

--- cell.h
+++ cell.h
@@ -2,5 +2,27 @@
 #ifndef CELL_H
 #define CELL_H
 
+typedef enum { cellId, cellConst, cellOpr } cellType;
+typedef struct cell{
+  enum cellType nodetype;
+  
+  union{
+    char* id;
+    int val;
+    struct { int oper;
+            int nbOfOper;
+            struct { struct cell *left, *right; char* operator; } operator;
+           } oper;
+  } body;
+} cell;
+
+
+  
+  
+  
+  
+    
+    
+  
 
 #endif
