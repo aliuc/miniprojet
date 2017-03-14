fa6fe3f0fa6d65e59db4e047de72e8c5d7563e4e
Luc Allio <luc.allio.3@gmail.com>
2017-03-14 20:30:47 +0100

    Update struct.c

--- struct.c
+++ struct.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "struct.h"
 
+nodeType* nbr (int val){
+  nodeType* p;
+  p->type = typeCOnstant;
+  p->val = val;
+  return p;
+}
+
+nodeType* id (char* id){
+  nodeType* p;
+  p->type = typeIdentifier;
+  p->id = id;
+  return p;
+}
+
+nodeType* opr (int operator, int nbOfOperators){
+  nodeType* p;
+  p->type = typeOperator;
+  p->opr.operator = operator;
+  p->opr.nbOfOperators = nbOfOperators;
+  return p;
+}
+
+
+  
